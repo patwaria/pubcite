6107a60ece0a2db36ec490c69df62897c66b2787
sachin19 <sachin19@3fde219a-4a57-491d-8d88-3e42e717fdc2>
2013-03-24 16:08:40 +0000

    new class named SettingsRecord used to read and save settings

---
+++ PubCite/PubCite/SettingsRecord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PubCite
+{
+    class SettingsRecord
+    {
+        private Settings set;
+        public SettingsRecord()
+        {
+            set = new Settings();
+        }
+        public Settings ReadSettings()
+        {
+            if (File.Exists(@"Settings"))
+            {// populate the list
+                FileStream fs = new FileStream(@"Settings", FileMode.Open, FileAccess.Read);
+                BinaryFormatter bf = new BinaryFormatter();
+                object inf = bf.Deserialize(fs);
+                set = (Settings)inf;
+                fs.Close();
+            }
+            return set;
+        }
+        public void SaveSettings(Settings s)
+        {
+            Stream stream = File.Open(@"Settings", FileMode.Create);
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            bFormatter.Serialize(stream, set);
+            stream.Close();
+        } 
+    }
+}
